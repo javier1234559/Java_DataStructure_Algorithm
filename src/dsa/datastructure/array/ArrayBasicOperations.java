71a5d458f4cb921c731b2625b297a9eac2732f8f
javier1234559 <nhat1234559@gmail.com>
2023-06-19 19:20:42 +0700

    complete search algorithm in array

--- src/dsa/datastructure/array/ArrayBasicOperations.java
+++ src/dsa/datastructure/array/ArrayBasicOperations.java
@@ -8,7 +8,7 @@ public class ArrayBasicOperations { //Format code Crt+Atl+ L
     static int SIZE;
 
     static void PreTestData() {
-        array = new int[]{1, 2, 3, 4, 5};
+        array = new int[]{1, 2, 5, 4, 3};
         SIZE = array.length;
     }
 
@@ -103,34 +103,88 @@ public class ArrayBasicOperations { //Format code Crt+Atl+ L
     }
 
     static int searchInArray(int element) {
-        //Linear Search Algorithm
-//        for(int i = 0 ; i < SIZE ; i++) { //O(n)
-//            if(array[i] == element) {
+//        //Linear Search Algorithm
+//        for (int i = 0; i < SIZE; i++) { //O(n)
+//            if (array[i] == element) {
 //                return i;
 //            }
 //        }
-
-        //Sentinel Linear Search Algorithm
-        int last = array[SIZE - 1];
-        array[SIZE - 1] = element;
-        int i = 0;
-        while(array[i] != element) { //O(n)
-            i++;
-        }
-        array[SIZE - 1] = last;
-        if(i < SIZE - 1 || array[SIZE - 1] == element ){
-            return i;
-        }
+//
+//        //Sentinel Linear Search Algorithm
+//        int last = array[SIZE - 1];
+//        array[SIZE - 1] = element;
+//        int i = 0;
+//        while (array[i] != element) { //O(n)
+//            i++;
+//        }
+//        array[SIZE - 1] = last;
+//        if (i < SIZE - 1 || array[SIZE - 1] == element) {
+//            return i;
+//        }
 
         //Binary Search Algorithm
+        //Array must be sorted
+//        Arrays.sort(array); //O(n log n)
+//        int low = 0, high = SIZE - 1;
+//        while (low <= high) { //O(log n)
+//            // use low + (high - low)/ 2 is better cuz can avoid overflow
+//            int mid = (low + high) / 2;
+//            if (array[mid] == element) {
+//                return mid;
+//            } else if (array[mid] < element) {
+//                low = mid + 1;
+//            } else if (array[mid] > element) {
+//                high = mid - 1;
+//            }
+//        }
 
+        //Recursive Binary Algorithm
+        return recursiveBinarySearch(0, SIZE, element);
 
+//        //Ternary Algorithm
+//        int low = 0, high = SIZE - 1;
+//        while (low <= high) {
+//            int thirdPart = (high - low) / 3;
+//            int mid1 = low + thirdPart;
+//            int mid2 = high - thirdPart;
+//
+//            if (array[mid1] == element) {
+//                return mid1;
+//            } else if (array[mid2] == element) {
+//                return mid2;
+//            } else if (array[mid1] > element) {
+//                high = mid1 - 1;
+//            } else if (array[mid1] < element && array[mid2] > element) {
+//                high = mid2 - 1;
+//                low = mid1 + 1;
+//            } else if (array[mid2] < element) {
+//                low = mid2 + 1 ;
+//            }
+//        }
+//        return -1;
+    }
 
+    static int recursiveBinarySearch(int low, int high, int element) {
+        if (low <= high) { // not get over high
+            int mid = (low + high) / 2;
+            if (array[mid] == element) {
+                return mid;
+            } else if (array[mid] < element) {
+                return recursiveBinarySearch(mid + 1, high, element);
+            } else if (array[mid] > element) {
+                return recursiveBinarySearch(low, mid - 1, element);
+            }
+        }
         return -1;
     }
 
     static void reverseArray() {
-        // TODO: Implement this method
+        // Iterate Reverse
+        for (int i = 0; i < SIZE / 2; i++) {
+            int temp = array[i];
+            array[i] = array[SIZE - 1 - i];
+            array[SIZE - 1 - i] = temp;
+        }
     }
 
     static void rotateArray(int steps) {
